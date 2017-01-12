d0f467e0e44cbbc723cb02d2ed84e03885460a61
SarnaKhmel <olsarnat@gmail.com>
2017-01-12 12:39:44 +0200

    start project

---
+++ src/main/java/com/example/AircraftApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftApplication.class, args);
+	}
+}
